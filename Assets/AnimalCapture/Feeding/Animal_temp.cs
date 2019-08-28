6cb523bb2994ffab314cda4b1b6e23a26d02a04a
Daichi-1637 <jouhou1637@gmail.com>
2019-08-28 12:17:58 +0900

    餌が投げれるようにした

---
+++ Assets/AnimalCapture/Feeding/Animal_temp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal_temp : MonoBehaviour
+{
+    /// <summary>
+    /// 餌と動物が触れたら消滅させる
+    /// </summary>
+    /// <param name="collision">特に気にする必要はない</param>
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision.gameObject.tag : " + collision.gameObject.tag);
+        if(collision.gameObject.tag == "feed")
+            Destroy(collision.gameObject);
+    }
+}
