9ad56009ecff7e741e089ec61db44de30549d675
Daichi-1637 <jouhou1637@gmail.com>
2019-08-30 14:08:19 +0900

    注視範囲のオブジェクトと注視距離による範囲の変更(未完成)の実装

---
+++ Assets/ViewingAngleManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using HoloToolkit.Unity.InputModule;
+
+public class ViewingAngleManager : MonoBehaviour
+{
+    /// <summary>
+    /// GazeManagerで注視入力の最大距離が設定できるため, 
+    /// その距離を取得するために使用
+    /// </summary>
+    private GazeManager gazeManager;
+
+    /// <summary>
+    /// GazeManagerから得た注視入力の最大距離から
+    /// オブジェクトの大きさを取得
+    /// </summary>
+    void Start()
+    {
+        this.transform.localScale = new Vector3(
+            gazeManager.MaxGazeCollisionDistance,
+            gazeManager.MaxGazeCollisionDistance,
+            gazeManager.MaxGazeCollisionDistance);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
