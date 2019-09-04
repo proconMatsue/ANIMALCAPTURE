22b385acc37a1b9c85d7665888b37166bc9b1b0b
Hikaru Sakamoto <pikka@HikarunoMacBook-puro.local>
2019-09-04 14:02:21 +0900

    餌Boxオブジェクトを生成する関数の作成

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -1,17 +1,17 @@
-﻿using System.Collections;
+﻿ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Generator : MonoBehaviour
 {
     public GameObject[] animalObject;//生成する動物オブジェクトを格納する配列
-    public GameObject[] foodObject;//生成する餌オブジェクトを格納する配列
+    public GameObject[] feedBoxObject;//生成する餌オブジェクトを格納する配列
     public GameObject[] effectObject;//生成するエフェクトオブジェクトを格納する配列
 
     public float intervalTime;//生成間隔のインターバルタイム設定用関数
 
     public int fieldAnimalLimit;
-    public int fieldFoodLimit;
+    public int fieldFeedBoxLimit;//フィールド場に出現させる餌boxの上限数
 
     public float fieldPozX;//フィールドの座標範囲
     public float fieldPozY;
@@ -43,7 +43,17 @@ public class Generator : MonoBehaviour
     public void GenerateAnimal(int num)//実際に動物を生成する関数 引数でanimalObjectの場所を指定
     {
         Instantiate(animalObject[num], new Vector3( UnityEngine.Random.Range(0,fieldPozX),
-         UnityEngine.Random.Range(0, fieldPozY),fieldPozZ),Quaternion.identity);//xy座標はランダムで生成
+         UnityEngine.Random.Range(0, fieldPozY),fieldPozZ),Quaternion.identity);//xy座標はフィールド範囲内でランダムで生成
+    }
+
+
+    public void GenerateFeedBox()//餌boxを生成する関数 引数でanimalObjectの場所を指定
+    {
+        while (CheckFieldObject("feedBox") < fieldFeedBoxLimit)//フィールド場の餌boxを数え、減っている数だけ生成する
+        {
+            Instantiate(feedBoxObject, new Vector3(UnityEngine.Random.Range(0, fieldPozX),
+             UnityEngine.Random.Range(0, fieldPozY), fieldPozZ), Quaternion.identity);//xy座標はランダムで生成
+        }
     }
 
 
