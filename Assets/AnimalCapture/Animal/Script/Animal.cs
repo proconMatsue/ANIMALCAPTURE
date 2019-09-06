c1937a5f608fecca25738900d1d80b746a00ce79
Daichi-1637 <jouhou1637@gmail.com>
2019-09-06 20:40:36 +0900

    画面遷移をあきらめた

---
+++ Assets/AnimalCapture/Animal/Script/Animal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    [SerializeField] private GameObject cameraPrefab;
+
+    /// <summary>
+    /// 餌と動物が触れたら消滅させる
+    /// </summary>
+    /// <param name="collision">特に気にする必要はない</param>
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision.gameObject.tag : " + collision.gameObject.tag);
+        var mytag = this.gameObject.tag;
+        var colltag = collision.gameObject.tag;
+
+        if (
+            (mytag == "cat" && colltag == "pike") ||
+            (mytag == "squirrel" && colltag == "acorn") ||
+            (mytag == "lion" && colltag == "meat") ||
+            (mytag == "" && colltag == "meat")
+            )
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
