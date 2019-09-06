8288685029698083a357a72ee15ee16e52d12991
Daichi-1637 <jouhou1637@gmail.com>
2019-09-06 16:20:25 +0900

    餌やりブランチとのマージ

--- Assets/AnimalCapture/Feeding/Script/FeedingManager.cs
+++ Assets/AnimalCapture/Feeding/Script/FeedingManager.cs
@@ -9,16 +9,22 @@ public class FeedingManager : MonoBehaviour
     //餌オブジェクトのプレハブ
     [SerializeField] private GameObject feedPrefab;
     //餌を射出するときのスピード
-    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float speed = 300.0f;
+    //オブジェクトを破壊するまでにかかる時間
+    [SerializeField] private float DestryTime = 20.0f;
 
     /// <summary>
     /// 自身(動物オブジェクト)が注視されたら,
     /// 餌を自身に向けて, 餌を投げる.
     /// (カメラが餌を投げる指示を出すのではなく, 動物オブジェクト自身から餌を投げるようにしている)
     /// </summary>
-    public void Feeing() {
-        var feedInstance = Instantiate<GameObject>(feedPrefab, camera.transform.position, camera.transform.rotation);
+    /*private void Feeing() {
+        GameObject feedInstance = Instantiate<GameObject>(InventoryFeed[0] , camera.transform.position, camera.transform.rotation);
+        //投げるゲームオブジェクトはリスとから消しておく
+        InventoryFeed.RemoveAt(0);
         feedInstance.GetComponent<Rigidbody>().AddForce(feedInstance.transform.forward * speed);
-        Destroy(feedInstance, 5f);
-    }
+
+        //一定時間フィールド上に存在する餌オブジェクトは削除するようにする.
+        Destroy(feedInstance, DestryTime);  
+    }*/
 }
