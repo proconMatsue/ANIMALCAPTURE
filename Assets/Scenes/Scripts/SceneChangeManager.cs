628dae9ece6927eb1124dbaa5822ae42e710321e
Hikaru Sakamoto <pikka@HikarunoMacBook-puro.local>
2019-09-08 12:40:07 +0900

    シーン遷移時にカメラがずれる不具合を修正

--- Assets/Scenes/Scripts/SceneChangeManager.cs
+++ Assets/Scenes/Scripts/SceneChangeManager.cs
@@ -10,11 +10,35 @@ public class SceneChangeManager : MonoBehaviour
     private UnityEvent OnSelectionCompleted = new UnityEvent();
     [SerializeField] private string NextScene = "stage01";
 
+
+    //GameObject gazeCircle = GameObject.Find("UISelectionBar");
+    GameObject MixedRealityCameraParent;
+
+    /*public void CallSceneChange()
+    {
+        
+        Invoke("SceneChange", 2);
+    }*/
+    private void Start()
+    {
+        MixedRealityCameraParent = GameObject.Find("MixedRealityCameraParent");
+    }
+
     /// <summary>
     /// 注視入力されたら画面を
     /// </summary>
     public void SceneChange()
     {
+        Destroy(MixedRealityCameraParent);
         SceneManager.LoadScene(NextScene);
+        //gazeCircle.SetActive(false);
     }
+
+
+    /*public void GetCameraPos()
+    {
+        GameObject camera = GameObject.Find("MixedRealityCamera");
+
+    }*/
+
 }
