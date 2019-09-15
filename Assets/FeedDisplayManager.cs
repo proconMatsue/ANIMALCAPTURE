71a762410fc61b89e035b848f8db516f58a62d5b
Daichi-1637 <jouhou1637@gmail.com>
2019-09-15 14:45:08 +0900

    所持する餌を表示できるようにした

---
+++ Assets/FeedDisplayManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedDisplayManager : MonoBehaviour
+{
+    //テキストを表示するためのオブジェクト
+    [SerializeField] private GameObject textObject;
+
+    //餌を管理するマネージャのオブジェクト
+    [SerializeField] private GameObject feedInventoryManager;
+
+    /// <summary>
+    /// 毎フレーム餌を表示する
+    /// </summary>
+    private void Update()
+    {
+        //FeedInventoryManagerにアタッチされているFeedInventory.csを取得
+        FeedInventory feedInventory = feedInventoryManager.GetComponent<FeedInventory>();
+        //取得したコンポーネントから表示するための関数を呼び出し
+        feedInventory.FeedDisplay(textObject);
+    }
+}
