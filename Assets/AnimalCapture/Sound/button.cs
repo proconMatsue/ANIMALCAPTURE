b67c2adc2d77b22c977cf1120610a342315bdb2d
Yuki Matsumoto <kireinayuuhi@outlook.jp>
2019-09-27 12:39:56 +0900

    スタート画面BGM,ゲーム画面動物モデルアニメーション設定(反映はされてない)

---
+++ Assets/AnimalCapture/Sound/button.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startbutton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		AudioSource = GetComponents<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if()////注視入力が完了したら
+		{
+			////buttonを鳴らす
+			AudioSource.PlayOneShot(button);
+		}
+    }
+}
