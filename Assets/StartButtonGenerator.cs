0d55a07202565d30833ad1abae7fcb432e7886c5
Daichi-1637 <jouhou1637@gmail.com>
2019-09-30 15:47:05 +0900

    スタートボタンの更新

--- Assets/StartButtonGenerator.cs
+++ Assets/StartButtonGenerator.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class StartButtonGenerator : MonoBehaviour
 {
-    [SerializeField]
-    [Tooltip("スタートボタンのオブジェクト")]
+    [SerializeField, Tooltip("スタートボタンのオブジェクト")]
     private GameObject StartButton;
+
+    [SerializeField, Tooltip("プレーヤとスタートボタンとの距離")]
+    [Range(0.0f, 5.0f)]
+    private float DistanceToPlayer = 1.0f;
 
     /// <summary>
     /// ゲームスタート時にスタートボタンを生成するようにする
@@ -25,7 +28,12 @@ public class StartButtonGenerator : MonoBehaviour
         GameObject start = Instantiate<GameObject>(
             StartButton, 
             this.gameObject.transform);
-        
+
+        //生成場所を原点にする
+        Vector3 start_vec = start.transform.position;
+        start_vec = new Vector3(0.0f, 0.0f, DistanceToPlayer);
+        start.transform.position = start_vec;
+
         //スタートボタンをマネージャの子オブジェクトとする
         start.gameObject.transform.parent = this.gameObject.transform;
     }
