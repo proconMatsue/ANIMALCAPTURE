726b67356ac67057760d57a8bc1c5d89803e2709
Yuki Matsumoto <kireinayuuhi@outlook.jp>
2019-10-01 11:56:14 +0900

    ゲーム中アニメーション(できていないかもしれない)

---
+++ Assets/Sound/Script/ClickStartButtonScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickStartButtonScript : MonoBehaviour
+{
+	public AudioSource startbutton_AudioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+		startbutton_AudioSource = GetComponent<AudioSource> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void StartButtonClick()
+	{
+		startbutton_AudioSource.PlayOneShot (startbutton_AudioSource.clip);
+	}
+}
