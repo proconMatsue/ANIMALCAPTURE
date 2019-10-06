5a2889a4c9e5d99b26c6bdfb084eef9fd4c3a02d
OkudaSatsuki <o.satsubon120905.os@icloud.com>
2019-10-06 13:51:52 +0900

    ボタン選択時のSEを鳴らすbuttonSE.csを追加　start,restart,exitボタン選択時にSEを鳴らすようにした

---
+++ Assets/AnimalCapture/BGM/Script/buttonSE.cs
@@ -0,0 +1,33 @@
+﻿//StartButtonGeneratorにGazeSelectorTarget.csをアタッチ
+//OnSelectionCompleted()から、buttonPlay()を呼び出す
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonSE : MonoBehaviour
+{
+    public bool DontDestroyEnabled = true;
+    public AudioClip button;//ボタン選択時に鳴らすSE
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(DontDestroyEnabled)
+            DontDestroyOnLoad(this);//シーンを遷移してもオブジェクトが消えないようにする
+
+        audioSource = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void buttonPlay()
+    {
+        audioSource.PlayOneShot(button);//buttonを1回鳴らす
+    }
+}
